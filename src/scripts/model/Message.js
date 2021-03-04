48e622ecd8d314331815268cc32fa058b7fca646
Audrey <ay.dubois@gmail.com>
2021-03-05 00:43:45 +0100

    [ADD] option pendu

--- src/scripts/model/Message.js
+++ src/scripts/model/Message.js
@@ -16,41 +16,43 @@ class MessageHolder {
 
 export default class Message {
 
-  id = null;
+    id = null;
 
-  userName = '';
+    userName = '';
 
-  userMessageHolder = {};
+    userMessageHolder = {};
 
-  botMessageHolderList = [];
+    botMessageHolderList = [];
 
-  option = '';
+    option = '';
 
-  constructor({ id = null,
-                  botName = 'Mr bot',
-                  userName = '',
-                  userMessage = '',
-                  botMessage = [],
-                  option = '' } = {}) {
+    constructor({
+        id = null,
+        botName = 'Mr bot',
+        userName = '',
+        userMessage = '',
+        botMessage = [],
+        option = ''
+    } = {}) {
 
-    this.id = id;
-    this.userName = userName;
-    this.option = option;
+        this.id = id;
+        this.userName = userName;
+        this.option = option;
 
-    this.userMessageHolder = this.getMessageHolder(userName, userMessage, false, false);
-    this.botMessageHolderList = botMessage.map(mess => this.getMessageHolder(botName, mess, true, option === "shifumi"));
+        this.userMessageHolder = this.getMessageHolder(userName, userMessage, false, false);
+        this.botMessageHolderList = botMessage.map(mess => this.getMessageHolder(botName, mess, true, option === "shifumi" || option === "pendu"));
 
-  }
+    }
 
-  getRandomMSSleepTime = (message = '') => (Math.random() + 1) * 1000 + message.length * 40;
+    getRandomMSSleepTime = (message = '') => (Math.random() + 1) * 1000 + message.length * 40;
 
-  getMessageHolder = (userName, message, isBot, isShifumi) => {
+    getMessageHolder = (userName, message, isBot, isShifumi) => {
 
-      const speakBB = new SpeakBubble(userName, message, isBot);
-      const el = speakBB.getEl();
-      const timeDelay = isShifumi ? 0 : this.getRandomMSSleepTime(message);
+        const speakBB = new SpeakBubble(userName, message, isBot);
+        const el = speakBB.getEl();
+        const timeDelay = isShifumi ? 0 : this.getRandomMSSleepTime(message);
 
-      return new MessageHolder(userName, message, el, timeDelay);
-  }
+        return new MessageHolder(userName, message, el, timeDelay);
+    }
 
 }
--- src/scripts/model/SpeakBubble.js
+++ src/scripts/model/SpeakBubble.js
@@ -1,5 +1,12 @@
+import fs from 'fs'
+const pendu1 = fs.readFileSync(__dirname + "/../../images/pendu1.png")
+const pendu2 = fs.readFileSync(__dirname + "/../../images/pendu2.png")
+const pendu3 = fs.readFileSync(__dirname + "/../../images/pendu3.png")
+const pendu4 = fs.readFileSync(__dirname + "/../../images/pendu4.png")
+const pendu5 = fs.readFileSync(__dirname + "/../../images/pendu5.png")
+const pendu6 = fs.readFileSync(__dirname + "/../../images/pendu6.png")
+const pendu7 = fs.readFileSync(__dirname + "/../../images/pendu7.png")
 export default class SpeakBubble {
-
     name = '';
 
     message = '';
@@ -15,8 +22,14 @@ export default class SpeakBubble {
 
         if (this.message.match(/jpeg$|jpg$|png$|gif$/)) {
             this.speakBubbleEl = document.createElement('img')
-            this.speakBubbleEl.setAttribute("src", this.message)
-            this.speakBubbleEl.style.maxWidth = "2000px"
+            if (this.message.match(/pendu/)) {
+                let pendu = this.getImage(this.message)
+                this.speakBubbleEl.src = `data:image/jpg;base64,${pendu.toString('base64')}`
+                this.speakBubbleEl.style.width = "12vw"
+            } else {
+                this.speakBubbleEl.setAttribute("src", this.message)
+            }
+            this.speakBubbleEl.style.maxWidth = "60vw"
 
         } else {
             this.speakBubbleEl = document.createElement('p');
@@ -35,7 +48,24 @@ export default class SpeakBubble {
             this.speakBubbleEl.prepend(nameEl)
         }
     }
-
+    getImage = (msg) => {
+        switch (msg) {
+            case "pendu1.png":
+                return pendu1
+            case "pendu2.png":
+                return pendu2
+            case "pendu3.png":
+                return pendu3
+            case "pendu4.png":
+                return pendu4
+            case "pendu5.png":
+                return pendu5
+            case "pendu6.png":
+                return pendu6
+            case "pendu7.png":
+                return pendu7
+        }
+    }
     getEl = () => this.speakBubbleEl;
 
 }
